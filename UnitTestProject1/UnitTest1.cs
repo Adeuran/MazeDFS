f05fb34fe22d8dfbadf1e5dc36d3f01ffd8393dc
Adeuran <hyunbum75@gmail.com>
2018-11-18 23:58:44 +0900

    Add files via upload

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MazeWork;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FileManager file = new FileManager();
+            file.LoadMap();
+        }
+    }
+}
